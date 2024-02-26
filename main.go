6fbc439093ef18eacf7b772cb348fc4a92fcfd1a
celine.bowen <celine.bowen@emalify.com>
2024-02-26 15:33:38 +0300

    serving html to the web

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-// fmt formats strings
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	// fmt.Println("Hello, World")
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Println(w, "Serving Web apps")
+	// })
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./home.html")
+	})
+
+	http.ListenAndServe(":3000", nil)
+
 }
