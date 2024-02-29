8f4ebd7e3381815aff8017c9582297a293088455
celine.bowen <celine.bowen@emalify.com>
2024-02-29 12:45:27 +0300

    added an import comment

--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-	"net/http"
-)
+// imports
+
+import "net/http"
 
 func main() {
-	// fmt.Println("Hello, World")
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Println(w, "Serving Web apps")
-	// })
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./home.html")
 	})
 
 	http.ListenAndServe(":3000", nil)
-
 }
